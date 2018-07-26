fc44bf631bfa9d5321d8f287a42fe84730c07940
Csilla Urban <csillaurban@gmail.com>
2018-07-26 13:29:58 +0200

    debug ships

--- main.py
+++ main.py
@@ -13,29 +13,27 @@ def main():
     player2_name = input('Second players name: ')
     
     # creating ships on player1's board
-    ship1_ver1 = gen_random_ship(board1, 1, 4, "ver")
-    ship1_ver2 = gen_random_ship(board1, 1, 3, "ver")
-    ship1_hor1 = gen_random_ship(board1, 1, 2, "hor")
-    ship1_hor2 = gen_random_ship(board1, 1, 3, "hor")
+    ship1_ver1 = gen_random_ship(board1, 4, "ver")
+    ship1_ver2 = gen_random_ship(board1, 3, "ver")
+    ship1_hor1 = gen_random_ship(board1, 2, "hor")
+    ship1_hor2 = gen_random_ship(board1, 3, "hor")
 
     # creating ships on player2's board
-    ship2_ver1 = gen_random_ship(board2, 1, 4, "ver")
-    ship2_ver2 = gen_random_ship(board2, 1, 3, "ver")
-    ship2_hor1 = gen_random_ship(board2, 1, 3, "hor")
-    ship2_hor2 = gen_random_ship(board2, 1, 2, "hor")
+    ship2_ver1 = gen_random_ship(board2, 4, "ver")
+    ship2_ver2 = gen_random_ship(board2, 3, "ver")
+    ship2_hor1 = gen_random_ship(board2, 3, "hor")
+    ship2_hor2 = gen_random_ship(board2, 2, "hor")
     
     print_board(board1)
 
     while True:
         print("\033[1;34;40m \n")
-        # print(ship1_ver1)
-        # print(ship1_ver2)
-        # print(ship1_hor1)
-        # print(ship1_hor2)
-        # print(ship2_ver1)
-        # print(ship2_ver2)
-        # print(ship2_hor1)
-        # print(ship2_hor2)
+        print(ship1_ver1)
+        print(ship1_ver2)
+        print(ship1_hor1)
+        print(ship2_ver1)
+        print(ship2_ver2)
+        print(ship2_hor2)
         
         # board, guessing, hit count for player1
         player(board1, player1_name)
@@ -61,8 +59,8 @@ def main():
         print_board(board2)
         if hit_ship(board2, ship2_ver1, 4, "ver") \
            or hit_ship(board2, ship2_ver2, 3, "ver") \
-           or hit_ship(board2, ship2_hor1, 3, "hor") \
-           or hit_ship(board2, ship2_hor2, 2, "hor"):
+           or hit_ship(board2, ship2_hor2, 2, "hor")\
+           or hit_ship(board2, ship2_hor1, 3, "hor"):
             print("It was a boat")
 
         # end play for player2
--- ships.py
+++ ships.py
@@ -1,38 +1,52 @@
 from random import randint
 
 
-def gen_random_ship(board, pieces, parts, direction=None):
+def gen_random_ship(board, parts, direction=None):
     ship_row = []
     ship_col = []
-    for _ in range(pieces):
+    gen_ship = True
+
+    while gen_ship:
         ship1_row = randint(0, len(board) - (parts + 1))
         ship1_col = randint(0, len(board) - (parts + 1))
-        
-        if board[ship1_row][ship1_col] != "Y" and board[ship1_row + (parts - 1)][ship1_col] != "Y":
-            board[ship1_row][ship1_col] = "Y"
-        elif board[ship1_row][ship1_col] != "Y" and board[ship1_row - (parts - 1)][ship1_col] != "Y":
-            board[ship1_row][ship1_col] = "Y"
-        elif board[ship1_row][ship1_col] != "Y" and board[ship1_row][ship1_col + (parts - 1)] != "Y":
-            board[ship1_row][ship1_col] = "Y"
-        elif board[ship1_row][ship1_col] != "Y" and board[ship1_row][ship1_col - (parts - 1)] != "Y":
-            board[ship1_row][ship1_col] = "Y"
-        else:
-            ship1_row = randint(0, len(board) - (parts + 1))
-            ship1_col = randint(0, len(board) - (parts + 1))
-        
-        ship_row.append(ship1_row)
-        ship_col.append(ship1_col)
+     
+        if direction == "ver":
+            if board[ship1_row][ship1_col] != "Y" \
+                and board[ship1_row + (parts - 1)][ship1_col] != "Y" \
+                and board[ship1_row + parts][ship1_col] != "Y" \
+                and board[ship1_row - 1][ship1_col] != "Y" \
+                and board[ship1_row][ship1_col - 1] != "Y" \
+                and board[ship1_row][ship1_col + 1] != "Y":
+                    board[ship1_row][ship1_col] = "Y"
+                    ship_row.append(ship1_row)
+                    ship_col.append(ship1_col)
+                    for i in range(parts - 1):
+                        ship_col.append(ship1_col)
+                        ship1_row += 1
+                        ship_row.append(ship1_row)
+                        board[ship1_row][ship1_col] = "Y"
+                    gen_ship = False    
+            else:
+                gen_ship = True 
 
-        for i in range(parts - 1):
-            if direction == "ver":
-                ship_col.append(ship1_col)
-                ship1_row += 1
-                ship_row.append(ship1_row)
-                board[ship1_row][ship1_col] = "Y"
-            elif direction == "hor":
-                ship_row.append(ship1_row) 
-                ship1_col += 1
-                ship_col.append(ship1_col)
-                board[ship1_row][ship1_col] = "Y"
+        elif direction == "hor":
+            if board[ship1_row][ship1_col] != "Y" \
+                and board[ship1_row][ship1_col + (parts - 1)] != "Y" \
+                and board[ship1_row][ship1_col + parts] != "Y" \
+                and board[ship1_row][ship1_col - 1] != "Y" \
+                and board[ship1_row - 1][ship1_col] != "Y" \
+                and board[ship1_row + 1][ship1_col] != "Y":
+                    board[ship1_row][ship1_col] = "Y"
+                    ship_row.append(ship1_row)
+                    ship_col.append(ship1_col)
+                    for i in range(parts - 1):
+                        ship_row.append(ship1_row) 
+                        ship1_col += 1
+                        ship_col.append(ship1_col)
+                        board[ship1_row][ship1_col] = "Y"
+                    gen_ship = False
+            else:
+                gen_ship = True 
+                
     return [ship_row, ship_col]           
 
